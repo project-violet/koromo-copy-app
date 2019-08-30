a645db291fa2bf9ffa689dca72575aa4292a127d
dc-koromo <koromo.software@gmail.com>
2019-08-30 21:30:20 +0900

    Init

---
+++ Koromo_Copy.Framework/Class1.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (C) 2019. dc-koromo. All Rights Reserved.
+// This source code is a part of Koromo Copy Project.
+
+using System;
+
+namespace Koromo_Copy.Framework
+{
+    public class Class1
+    {
+    }
+}

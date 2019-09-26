3a5caab2424c017a3cd8dc23b50771e043292ce7
dc-koromo <koromo.software@gmail.com>
2019-09-26 22:42:18 +0900

    UI-Desktop: Add UI Packages

--- Koromo_Copy.UI.Desktop/MainWindow.xaml.cs
+++ Koromo_Copy.UI.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// This source code is a part of Koromo Copy Project.
+// Copyright (C) 2019. dc-koromo. Licensed under the MIT Licence.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +21,7 @@ namespace Koromo_Copy.UI.Desktop
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MWindowLib.MetroWindow
     {
         public MainWindow()
         {

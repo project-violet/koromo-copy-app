390dac8e308fb0879d109e823ca92769391061e3
dc-koromo <koromo.software@gmail.com>
2019-09-26 22:35:53 +0900

    Crypto: Implements RSA Provider

---
+++ Koromo_Copy.Framework/Crypto/RSAHelper.cs
@@ -0,0 +1,39 @@
+﻿// This source code is a part of Koromo Copy Project.
+// Copyright (C) 2019. dc-koromo. Licensed under the MIT Licence.
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Koromo_Copy.Framework.Crypto
+{
+    public class RSAHelper
+    {
+        /// <summary>
+        /// Create new private key, pulibc key pair.
+        /// </summary>
+        /// <returns></returns>
+        public static (string, string) CreateKey()
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+            var private_key = RSAKeyExtensions.ToXmlString(rsa, true);
+            var public_key = RSAKeyExtensions.ToXmlString(rsa, false);
+            return (private_key, public_key);
+        }
+
+        public static byte[] Encrypt(byte[] target, string public_key)
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+            RSAKeyExtensions.FromXmlString(rsa, public_key);
+            return rsa.Encrypt(target, false);
+        }
+
+        public static byte[] Decrypt(byte[] target, string private_key)
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+            RSAKeyExtensions.FromXmlString(rsa, private_key);
+            return rsa.Decrypt(target, false);
+        }
+    }
+}

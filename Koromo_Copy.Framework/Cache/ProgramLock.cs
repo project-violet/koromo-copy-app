04c6513f2426d14c066d82c578fbeecda939a75c
dc-koromo <koromo.software@gmail.com>
2019-09-27 11:58:07 +0900

    Cache: Fix ProgramLock stream alive after lock file created

--- Koromo_Copy.Framework/Cache/ProgramLock.cs
+++ Koromo_Copy.Framework/Cache/ProgramLock.cs
@@ -1,6 +1,7 @@
 ﻿// This source code is a part of Koromo Copy Project.
 // Copyright (C) 2019. dc-koromo. Licensed under the MIT Licence.
 
+using Koromo_Copy.Framework.Log;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +13,7 @@ namespace Koromo_Copy.Framework.Cache
     {
         public const string Name = "koromo-copy.lock";
         public static FileStream LockStream;
+        public static bool ProgramCrashed = false;
 
         /// <summary>
         /// Create Lock File
@@ -23,21 +25,26 @@ namespace Koromo_Copy.Framework.Cache
             {
                 var full_path = Path.Combine(Directory.GetCurrentDirectory(), Name);
                 if (!File.Exists(full_path))
-                    File.Create(full_path);
+                    using (File.Create(full_path)) { }
+                else
+                    ProgramCrashed = true;
                 LockStream = new FileStream(full_path, FileMode.Open, FileAccess.Read, FileShare.None);
                 return true;
             }
-            catch (FileNotFoundException)
+            catch (FileNotFoundException e)
             {
                 // Write Permission Error
+                Logs.Instance.Push("[Program Lock] Locking pp-error - " + e.Message);
                 return false;
             }
-            catch (IOException)
+            catch (IOException e)
             {
+                Logs.Instance.Push("[Program Lock] Locking io-error - " + e.Message);
                 return false;
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                Logs.Instance.Push("[Program Lock] Locking unhandled-error - " + e.Message);
                 return false;
             }
         }

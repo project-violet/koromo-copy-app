e9d21b9aa5a3fc0194806fc05b88393d7152eba2
dc-koromo <koromo.software@gmail.com>
2019-09-30 00:20:07 +0900

    PP/FFmpeg: Add FFmpegPP header

---
+++ Koromo_Copy.Framework/Postprocessor/FFmpegPostprocessor.cs
@@ -0,0 +1,13 @@
+﻿// This source code is a part of Koromo Copy Project.
+// Copyright (C) 2019. dc-koromo. Licensed under the MIT Licence.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koromo_Copy.Framework.Postprocessor
+{
+    public class FFmpegPostprocessor
+    {
+    }
+}

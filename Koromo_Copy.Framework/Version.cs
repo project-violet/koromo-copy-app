11d60d7ce5014d097b65af58590a4789c2d4c55f
dc-koromo <koromo.software@gmail.com>
2019-10-12 19:29:34 +0900

    Version: Update version

--- Koromo_Copy.Framework/Version.cs
+++ Koromo_Copy.Framework/Version.cs
@@ -11,7 +11,7 @@ namespace Koromo_Copy.Framework
     {
         public const int MajorVersion = 2019;
         public const int MinorVersion = 10;
-        public const int BuildVersion = 5;
+        public const int BuildVersion = 12;
 
         public const string Name = "Koromo Copy";
         public static string Text { get; } = $"{MajorVersion}.{MinorVersion}.{BuildVersion}";

0b681cf3b69dfd68b41639ff65a883adc2317867
dc-koromo <koromo.software@gmail.com>
2019-12-29 19:33:24 +0900

    Jmana: Add copyright comment

--- Koromo_Copy.Framework/Extractor/JmanaExtractor.cs
+++ Koromo_Copy.Framework/Extractor/JmanaExtractor.cs
@@ -1,4 +1,7 @@
-﻿using HtmlAgilityPack;
+﻿// This source code is a part of Koromo Copy Project.
+// Copyright (C) 2019. dc-koromo. Licensed under the MIT Licence.
+
+using HtmlAgilityPack;
 using Koromo_Copy.Framework.Network;
 using Koromo_Copy.Framework.Utils;
 using System;

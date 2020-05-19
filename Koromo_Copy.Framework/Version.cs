b9e63305d81d80701f52a8bf684f1487f14ca211
violet-dev <violet.dev.master@gmail.com>
2020-05-19 10:00:01 +0900

    Version: Update version

--- Koromo_Copy.Framework/Version.cs
+++ Koromo_Copy.Framework/Version.cs
@@ -9,9 +9,9 @@ namespace Koromo_Copy.Framework
 {
     public class Version
     {
-        public const int MajorVersion = 2019;
-        public const int MinorVersion = 12;
-        public const int BuildVersion = 30;
+        public const int MajorVersion = 2020;
+        public const int MinorVersion = 05;
+        public const int BuildVersion = 19;
 
         public const string Name = "Koromo Copy";
         public static string Text { get; } = $"{MajorVersion}.{MinorVersion}.{BuildVersion}";
